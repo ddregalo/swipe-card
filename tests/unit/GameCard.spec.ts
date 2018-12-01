c0bac55e044f183bbadd0417c6324f93a9279f08
danielcampos <ddregalo@gmail.com>
2018-12-01 13:28:19 +0000

    Test card start at center

--- tests/unit/GameCard.spec.ts
+++ tests/unit/GameCard.spec.ts
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils';
 import GameCard from '@/components/GameCard.vue';
 
 describe('GameCard.vue', () => {
@@ -19,4 +19,34 @@ describe('GameCard.vue', () => {
     });
     expect(wrapper.props().isCurrent).toBe(true);
   });
+
+  it('card starts at center position with no rotation', () => {
+    const card = 'test';
+    const isCurrent = true;
+    const wrapper = mount(GameCard, {
+      propsData: { card, isCurrent },
+    });
+    expect(wrapper.vm.$data.interactPosition.rotation).toBe(0);
+    expect(wrapper.vm.$data.interactPosition.x).toBe(0);
+    expect(wrapper.vm.$data.interactPosition.y).toBe(0);
+  });
+
+  describe('Lifecycle Hooks', () => {
+  // it('Mounted Lifecycle Hook ', done => {
+  //     const card = 'test';
+  //     const isCurrent = true;
+  //     const wrapper = shallowMount(GameCard, {
+  //       propsData: { card, isCurrent },
+  //     });
+  //     const sinon = require('sinon');
+  //     const elementInteractStub = sinon.stub(wrapper.vm.$mount());
+  //     // const addEventStub = sinon.stub(window, 'addEventListener');
+
+  //     expect(elementInteractStub).toBeCalledWith(wrapper.vm);
+  //     elementInteractStub.restore();
+  //     // addEventStub.restore()
+  //     done();
+  // });
+  });
+
 });
